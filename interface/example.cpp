737b784cd09c702f94179aefb37c45f5e0d72f99
IgorCJ <igor.junqueira@fgv.edu.br>
2020-05-29 17:33:28 -0300

    php com c++

---
+++ interface/example.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "funciona!\n";
+    return 0;
+}
--- interface/search.php
+++ interface/search.php
@@ -2,7 +2,10 @@
 
 if(isset($_POST['search_key']) && !empty($_POST['search_key'])){
     $word = addslashes($_POST['search_key']);
-    include 'results.html';
+    exec("./example.out $word", $output);
+    $test = "testando";
+    echo "$output $test";
+    //include 'results.html';
 }
 else{
     echo("Erro no código...");
