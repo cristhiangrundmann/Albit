ca586c59a800e14237df186318b7eeee04bf112a
IgorCJ <igor.junqueira@fgv.edu.br>
2020-05-30 11:25:45 -0300

    integração cpp em php

--- interface/example.cpp
+++ interface/example.cpp
@@ -2,11 +2,12 @@
 #include<string.h>
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
     string titulos[20];
     for(int i=0;i<20; i++){
         titulos[i]="Título do texto " + to_string(i);
     }
+    cout << 18 << endl;
     int index[3]={10,16,8};
     for(int i=0; i<3; i++){
         cout << index[i] << " => ";
--- interface/search.php
+++ interface/search.php
@@ -1,27 +1,16 @@
 <?php
 
 if(isset($_POST['search_key']) && !empty($_POST['search_key'])){
-    $word = addslashes($_POST['search_key']);
-    exec("./example.out $word", $output);
-    //$results = $output
-    //for($i=0;$i<count($output);$i++){
-        //echo($output[$i]);
-        //echo("\n");
-        //$array="1";
-    //}
+    exec("./example.out '" . $_POST['search_key'] . "'", $output);
+    $time=$output[0];
+    unset($output[0]);
     foreach ($output as $i => $value) {
         $output[$i]=explode(' => ',$output[$i]);
         $results[$output[$i][0]]=$output[$i][1];
     }
     unset($output);
-
-    //$results = explode(' => ',$output[0]);
-
-    //$array = array($results[0] => $results[1]);
-
-    //var_dump($results);
     print_r($results);
-    //include 'results.html';
+    echo($time);
 }
 else{
     include 'index.html';
