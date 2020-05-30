6f52165de32a4283966d1862680b71b234617b8e
IgorCJ <igor.junqueira@fgv.edu.br>
2020-05-30 12:36:01 -0300

    resultados para programar páginas na interface

--- interface/example.cpp
+++ interface/example.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    string titulos[20];
-    for(int i=0;i<20; i++){
+    string titulos[30];
+    for(int i=0;i<30; i++){
         titulos[i]="Título do texto " + to_string(i);
     }
     cout << 18 << endl;
-    int index[3]={10,16,8};
-    for(int i=0; i<3; i++){
+    int index[30]={10,9,8,7,6,5,4,3,2,1,20,19,18,17,16,15,14,13,12,11,30,29,28,27,26,25,24,23,22,21};
+    for(int i=0; i<30; i++){
         cout << index[i] << " => ";
-        cout << titulos[index[i]] << endl;
+        cout << titulos[index[i]-1] << endl;
     }
     return 0;
 }
