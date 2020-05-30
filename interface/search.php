e56961f8b6528781e42e254bd33c5acf83970eba
IgorCJ <igor.junqueira@fgv.edu.br>
2020-05-30 16:43:06 -0300

    php

--- interface/search.php
+++ interface/search.php
@@ -1,21 +1,33 @@
 <?php
 
-if(isset($_POST['search_key']) && !empty($_POST['search_key'])){
-    exec("./example.out '" . $_POST['search_key'] . "'", $results);
-    $time=$results[0];
-    unset($results[0]);
-    foreach ($results as $i => $value) {
-        $results[$i]=explode(' => ',$results[$i]);
+if (isset($_POST['search_key'])) {
+    if(!empty($_POST['search_key'])){
+        exec("./example.out '" . $_POST['search_key'] . "'", $results);
+        $time=$results[0];
+        unset($results[0]);
+        foreach ($results as $i => $value) {
+            $results[$i]=explode(' => ',$results[$i]);
+        }
+        $qtd=count($results);
+        $atual=1;
+        $pgs=ceil($qtd/10);
+        include 'results.html';
     }
-    $qtd=count($results);
-    $atual=1;
-    $pgs=ceil($qtd/20);
-    include 'results.html';
+    else{
+        include 'index.html';
+        echo "<script> confirm('Insira uma pesquisa válida?');</script>";
+    }
 }
-else{
-    include 'index.html';
-    echo "<script> confirm('Insira uma pesquisa válida?');</script>";
-}
+
+
+    if (isset($_POST['previous'])) {
+        echo("anterior");
+        echo("$atual");
+    }
+    if (isset($_POST['next'])) {
+        echo("pŕoxima");
+        echo("$atual");
+    }
 
 
 ?>
