322fc80709119c1500bb30f67d2bb5266f2f8e34
IgorCJ <igor.junqueira@fgv.edu.br>
2020-05-30 11:00:13 -0300

    php com c++

--- interface/example.cpp
+++ interface/example.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include<string.h>
 using namespace std;
 
 int main(){
-    cout << "funciona!\n";
+    string titulos[20];
+    for(int i=0;i<20; i++){
+        titulos[i]="Título do texto " + to_string(i);
+    }
+    int index[3]={10,16,8};
+    for(int i=0; i<3; i++){
+        cout << index[i] << " => ";
+        cout << titulos[index[i]] << endl;
+    }
     return 0;
 }
--- interface/search.php
+++ interface/search.php
@@ -3,9 +3,25 @@
 if(isset($_POST['search_key']) && !empty($_POST['search_key'])){
     $word = addslashes($_POST['search_key']);
     exec("./example.out $word", $output);
-    //$test = "testando";
-    //echo "$output $test";
-    include 'results.html';
+    //$results = $output
+    //for($i=0;$i<count($output);$i++){
+        //echo($output[$i]);
+        //echo("\n");
+        //$array="1";
+    //}
+    foreach ($output as $i => $value) {
+        $output[$i]=explode(' => ',$output[$i]);
+        $results[$output[$i][0]]=$output[$i][1];
+    }
+    unset($output);
+
+    //$results = explode(' => ',$output[0]);
+
+    //$array = array($results[0] => $results[1]);
+
+    //var_dump($results);
+    print_r($results);
+    //include 'results.html';
 }
 else{
     include 'index.html';
