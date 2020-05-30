1f003b1f042240619a90732b45da14e008becb5d
IgorCJ <igor.junqueira@fgv.edu.br>
2020-05-30 10:06:05 -0300

    tratativa de pesquisa

--- interface/search.php
+++ interface/search.php
@@ -3,12 +3,13 @@
 if(isset($_POST['search_key']) && !empty($_POST['search_key'])){
     $word = addslashes($_POST['search_key']);
     exec("./example.out $word", $output);
-    $test = "testando";
-    echo "$output $test";
-    //include 'results.html';
+    //$test = "testando";
+    //echo "$output $test";
+    include 'results.html';
 }
 else{
-    echo("Erro no código...");
+    include 'index.html';
+    echo "<script> confirm('Insira uma pesquisa válida?');</script>";
 }
 
 
