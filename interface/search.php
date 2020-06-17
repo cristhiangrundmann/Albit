ca57182569aee97dca903ddcb92bbd4b6a3bf02a
IgorCJ <igor.junqueira@fgv.edu.br>
2020-06-17 10:42:13 -0300

    troca do método de pesquisa para GET

--- interface/search.php
+++ interface/search.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 function pesquisar($search_key){
     exec("./example.out '" . $search_key . "'", $results);
     $time=$results[0];
@@ -13,37 +15,18 @@ function pesquisar($search_key){
             $results[$i]=explode(' => ',$results[$i]);
         }
         $qtd=count($results);
-        $atual=1;
-        $pgs=ceil($qtd/10);
         include 'results.html';
     }
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['search_key'])) {
-        if(!empty($_POST['search_key'])){
-            pesquisar($_POST['search_key']);
-        }
-        else{
-            include 'index.html';
-            echo "<script> confirm('Insira uma pesquisa válida?');</script>";
-        }
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if (!empty($_GET['word'])){
+        pesquisar($_GET['word']);
     }
-
-
-        if (isset($_POST['previous'])) {
-            $atual = max($atual-1,1);
-            include 'results.html';
-        }
-
-        if (isset($_POST['next'])) {
-            $atual = max($atual+1,pgs);
-            include 'results.html';
-        }
-
-}
-else{
-    pesquisar($_GET['sugestion']);
+    else{
+        include 'index.html';
+        echo "<script> confirm('Insira uma pesquisa válida?');</script>";
+    }
 }
 
 ?>
